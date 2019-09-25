6485644b5f32d321be1be6ecb507a2fc6b3b8bf6
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-25 06:13:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = Hash.new
+  data.each do |category, value|
+    value.each do |choices, names_array|
+      names_array.each do |name|
+        if new_hash[name] == nil
+          new_hash[name] = {}
+        end
+        if new_hash[name][category] == nil
+          new_hash[name][category] = []
+        end
+        new_hash[name][category] << choices.to_s
+      end
+    end
+  end
+  new_hash
 end
