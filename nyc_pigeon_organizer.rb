3988d3f9cb7654681ee5be60aca124ce12b0c464
steadfast-motherboard-7317 <imadejr@yahoo.com>
2019-10-22 22:59:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    pigeon_hash = {}
+    
+  data.each do |pidgeon_data, value|
+    value.each do |stats, all_names|
+      all_names.each do |name|
+        if !pigeon_hash[name]
+          pigeon_hash[name] = {}
+        end
+        
+        if !pigeon_hash[name][pidgeon_data] 
+          pigeon_hash[name][pidgeon_data] = []
+        end
+        pigeon_hash[name][pidgeon_data].push(stats.to_s)
+      end
+    end 
+  end
+  
+  pigeon_hash
+
 end
