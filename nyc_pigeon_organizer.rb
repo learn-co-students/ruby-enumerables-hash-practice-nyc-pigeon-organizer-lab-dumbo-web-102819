1f912670acf44606b8423ed35a0361e111b298d1
Guligena Aierken <guligena_aierken@163.com>
2019-10-23 19:55:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,32 +1,21 @@
-require "pry"
-<<<<<<< HEAD
-
 def nyc_pigeon_organizer(data)
-  pegion_list = {}
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
 
-    data.each do |key, value|
-      value.each do |color,name|
-        name.each do |pegion|
-          pegion_list[pegion] ||= {}
-          pegion_list[pegion][key] ||= []
-          pegion_list[pegion][key] << color.to_s
+        if !new_hash[name]
+          new_hash[name] = {}
         end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+
+        new_hash[name][key] << new_value.to_s
+
       end
     end
-    pegion_list
-=======
-def nyc_pigeon_organizer(data)
-      new_hash = {}
-      data.each do |key, value|
-        value.each do |color,name|
-          binding.pry
-
-        end
-      end
-
-
-
-
-
->>>>>>> 82e722da40588914eef86b8e2117717c0ba859de
+  end
+  new_hash
 end
