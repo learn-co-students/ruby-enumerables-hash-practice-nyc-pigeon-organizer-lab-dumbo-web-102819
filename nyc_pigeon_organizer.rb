9699737c907979e0699ce94b55ac915fe9e7b289
Guligena Aierken <guligena_aierken@163.com>
2019-10-23 14:10:44 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pegion_list = {}
+
+    data.each do |key, value|
+      value.each do |color,name|
+        name.each do |pegion|
+          pegion_list[pegion] ||= {}
+          pegion_list[pegion][key] ||= []
+          pegion_list[pegion][key] << color.to_s
+        end
+      end
+    end
+    pegion_list
 end
